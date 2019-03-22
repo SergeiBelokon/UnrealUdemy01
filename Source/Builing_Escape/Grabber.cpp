ff011e3a4786f6169e68d8522f56f3c97ff0ace5
Sergei Belokon <sergei.e.belokon@gmail.com>
2019-03-22 20:07:57 +0200

    Refactoring

--- Source/Builing_Escape/Grabber.cpp
+++ Source/Builing_Escape/Grabber.cpp
@@ -22,7 +22,8 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber is Active"));
-	
+    FindPhysicsComponent();
+    FindInputComponent();
 }
 
 
@@ -31,31 +32,77 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+    
+    
+    
+}
+
+void UGrabber::FindPhysicsComponent()
+{
+    PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+    if (PhysicsHandle)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PhysicsHandle is Active"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("PhysicsHandle is not found"));
+    }
+}
+
+void UGrabber::FindInputComponent()
+{
+    InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+    if (InputComponent)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("InputComponent is Active"));
+        InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+        InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("InputComponent is not found"));
+    }
+}
+
+void UGrabber::Grab()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Grag func is called"));
+    GetFirstPhysicsBodyInReach();
+}
+
+void UGrabber::Release()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Release func is called"));
+}
+
+const FHitResult UGrabber::GetFirstPhysicsBodyInReach()
+{
     FVector PlayerViewPointLocation;
     FRotator PlayerViewPointRotation;
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
        OUT PlayerViewPointLocation,
        OUT PlayerViewPointRotation
     );
     /*
-    UE_LOG(LogTemp, Warning, TEXT("Location is %s, Rotation is %s"),
-       *PlayerViewPointLocation.ToString(),
-       *PlayerViewPointRotation.ToString()
+     UE_LOG(LogTemp, Warning, TEXT("Location is %s, Rotation is %s"),
+         *PlayerViewPointLocation.ToString(),
+         *PlayerViewPointRotation.ToString()
+     );
+     */
+    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+    /*
+    DrawDebugLine(
+        GetWorld(),
+        PlayerViewPointLocation,
+        LineTraceEnd,
+        FColor(255, 0, 0),
+        false,
+        0.f,
+        0.f,
+        10.f
     );
     */
-    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
-    
-    DrawDebugLine(
-      GetWorld(),
-      PlayerViewPointLocation,
-      LineTraceEnd,
-      FColor(255, 0, 0),
-      false,
-      0.f,
-      0.f,
-      10.f
-    );
-    
     FHitResult Hit;
     FCollisionQueryParams TraceParametrs(TEXT(""), false, GetOwner());
     
@@ -73,7 +120,5 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
         FString HitObjectName = HitActor->GetName();
         UE_LOG(LogTemp, Warning, TEXT("We hit a %s"), *HitObjectName);
     }
-    
-    
+    return FHitResult();
 }
-
